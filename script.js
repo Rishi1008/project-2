1e54ae96c38f31334163a6dc65cdfff694048330
Rishi1008 <113489385+Rishi1008@users.noreply.github.com>
2024-05-06 08:29:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 import LocomotiveScroll from 'locomotive-scroll';
 
 const scroll = new LocomotiveScroll({
-  el: document.querySelector('#main'),
-  smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
 });
-
+function firstPageAnim(){
+    var tl = gsap.timeline();
+    tl.from("#nav",{
+      y:'-10',
+      opacity:0,
+      duration: 1.5,
+      ease : Expo.easeInOut
+    })
+    .to(".boundingelem",{
+      y:0,
+      ease : Expo.easeInOut,
+      duration:2,
+      stragger :.2
+    })
+    .from("#herofooter",{
+      y:-10,
+      opacity :0,
+      duration :1.5,
+      delay: -.5,
+      ease:Expo.easeInOut
+    })
+}
 function circleMouseFollower(xscale, yscale) {
   window.addEventListener("mousemove", function (dets) {
     document.querySelector(
@@ -12,3 +33,77 @@ function circleMouseFollower(xscale, yscale) {
     ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
   });
 }
+circleMouseFollower();
+firstPageAnim();import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+function firstPageAnim(){
+    var tl = gsap.timeline();
+    tl.from("#nav",{
+      y:'-10',
+      opacity:0,
+      duration: 1.5,
+      ease : Expo.easeInOut
+    })
+    .to(".boundingelem",{
+      y:0,
+      ease : Expo.easeInOut,
+      duration:2,
+      stragger :.2
+    })
+    .from("#herofooter",{
+      y:-10,
+      opacity :0,
+      duration :1.5,
+      delay: -.5,
+      ease:Expo.easeInOut
+    })
+}
+function circleMouseFollower(xscale, yscale) {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      "#minicircle"
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+  });
+}
+circleMouseFollower();
+firstPageAnim();import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+function firstPageAnim(){
+    var tl = gsap.timeline();
+    tl.from("#nav",{
+      y:'-10',
+      opacity:0,
+      duration: 1.5,
+      ease : Expo.easeInOut
+    })
+    .to(".boundingelem",{
+      y:0,
+      ease : Expo.easeInOut,
+      duration:2,
+      stragger :.2
+    })
+    .from("#herofooter",{
+      y:-10,
+      opacity :0,
+      duration :1.5,
+      delay: -.5,
+      ease:Expo.easeInOut
+    })
+}
+function circleMouseFollower(xscale, yscale) {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      "#minicircle"
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+  });
+}
+circleMouseFollower();
+firstPageAnim();
